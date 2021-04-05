0b42a7a474d77d8949e538528fb047881b0ac123
alekskarrrl <alekskarrrl@gmail.com>
2021-04-05 05:54:45 +0300

    Add config

---
+++ config.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 24 04:05:02 2021
+@author: User
+"""
+
+bot_token = BOT_TOKEN
+
+bot_chatID = BOT_GROUP_CHAT_ID
+
+
+#bot_chatID = BOT_CHAT_ID
+
+# To get group chat id:
+#https://api.telegram.org/bot<bot_token>/getUpdates
